d5cd3c3a08d33f782c0999c9295ac81a7b263e3b
nhatminhmibo <minhnguyen2259@gmail.com>
2021-12-01 15:42:26 +0700

    first commit

---
+++ src/test/java/stackjava/com/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package stackjava.com.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
